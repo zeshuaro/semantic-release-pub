e37bfc54f9a91bfbcda1e7a1f15156aa1ad1676c
Joshua Tang <joshuaystang@gmail.com>
2023-09-19 23:38:12 +1000

    fix: `prepare` lifecycle and release workflow to build plugin

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,6 +1,5 @@
 import { readFileSync, writeFileSync } from "fs";
-import SemanticReleaseError from "@semantic-release/error";
-import { Context } from "semantic-release";
+import { PrepareContext } from "semantic-release";
 import { parse } from "yaml";
 import { Pubspec } from "./schemas.js";
 import { PluginConfig } from "./types.js";
@@ -8,22 +7,17 @@ import { getConfig } from "./utils.js";
 
 export const prepare = async (
   pluginConfig: PluginConfig,
-  { nextRelease }: Context,
+  { nextRelease }: PrepareContext,
 ) => {
   const { pubspecPath } = getConfig(pluginConfig);
-  const version = nextRelease?.version;
-
-  if (!version) {
-    throw new SemanticReleaseError(
-      "Could not determine version from semantic release",
-    );
-  }
 
   const data = readFileSync(pubspecPath, "utf-8");
   const pubspec = Pubspec.parse(parse(data));
+
   const newData = data.replace(
     new RegExp(`^version:.*${pubspec.version}`),
-    `version: ${version}`,
+    `version: ${nextRelease.version}`,
   );
+
   writeFileSync(pubspecPath, newData);
 };
