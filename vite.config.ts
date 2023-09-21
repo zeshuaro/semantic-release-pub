8ff33d5a2e19a4653e73cef06c819a9b3e9597c3
Joshua Tang <joshuaystang@gmail.com>
2023-09-22 00:15:08 +1000

    chore: cleanup files from biome (#26)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
-      reporter: [["html"], ["text"], ["lcov", { projectRoot: "src" }]],
-    },
-  },
+      reporter: [['html'], ['text'], ['lcov', { projectRoot: 'src' }]]
+    }
+  }
 });
