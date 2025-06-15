4ab9ed38959de2e3df048a7aee5161b8524c3544
Joshua Tang <joshuaystang@gmail.com>
2025-06-15 18:31:45 +1000

    chore: add strictest and node-ts tsconfigs (#787)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { verifyConditions } from "./verifyConditions.js";
-export { PluginConfig } from "./types.js";
 export { prepare } from "./prepare.js";
 export { publish } from "./publish.js";
+export type { PluginConfig } from "./types.js";
+export { verifyConditions } from "./verifyConditions.js";
